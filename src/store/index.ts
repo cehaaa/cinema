b9c019651a78a3187d4ddabcf0ed2ac7c351b4e3
christianngouw <christiangouw168@gmail.com>
2021-09-20 20:25:43 +0700

    chore: code refactoring :hammer:

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,56 +4,16 @@ import {usersStore} from './users'
 
 export default createStore({
   state: {
-    friends : [
-      {
-        name : "Luigi",
-        profile_image : "220px-Ant-Man_poster.jpg",
-        online_status : true
-      },
-      {
-        name : "Jhon",
-        profile_image : "TheAvengers2012Poster.jpg",
-        online_status : false
-      }
-    ],
-    friendSuggestions : [
-      {
-        name : "Mikey",
-        profile_image : "220px-Ant-Man_poster.jpg",
-        online_status : true
-      },
-      {
-        name : "Kazutora",
-        profile_image : "220px-Ant-Man_poster.jpg",
-        online_status : false
-      },
-      {
-        name : "Luffy",
-        profile_image : "220px-Ant-Man_poster.jpg",
-        online_status : true
-      },
-      {
-        name : "Kobayasi",
-        profile_image : "220px-Ant-Man_poster.jpg",
-        online_status : false
-      },    
-    ]    
   },
   getters : {
-    friends : (state) => {
-      return state.friends
-    },
-    friendSuggestions : (state) => {
-      return state.friendSuggestions
-    }
   },
   mutations: {
-    
   },
   actions: {
   },
   modules: {
     "movies-store" : moviesStore,
     "users-store": usersStore
+    
   }
 })
--- src/store/users.ts
+++ src/store/users.ts
@@ -2,23 +2,63 @@ import axios from 'axios'
 export const usersStore = {
   namespaced : true,
   state : () => ({
-    users : []
+    users : [],
+    friends : [],
+    suggestedFriends : [],
   }),
   getters: {
     users(state:any){
       return state.users
+    },
+    friends(state:any){
+      return state.friends
+    },
+    suggestedFriends(state:any){
+      return state.suggestedFriends
     }
   },
   mutations: {
     SET_USERS(state:any, data:any){
       state.users = data
+    },
+    SET_FRIENDS(state:any, data:any){
+      state.friends = data
+    },
+    SET_SUGGESTED_FRIENDS(state:any, data:any){
+      state.suggestedFriends = data
     }
   },
   actions : {
-    fetchUsers( {commit}:any ){
+    fetchUsers( {commit}:any,){
       axios.get('/users')
         .then(res => {
           commit('SET_USERS', res.data);
+        })
+        .catch(err => {
+          console.error(err);
+        })
+    },
+    fetchFriends ({commit}:any ){
+      axios.get('/users', {
+        params : {
+          is_friend : true
+        }
+      })
+        .then(res => {
+          commit('SET_FRIENDS', res.data)
+        })
+        .catch(err =>{
+          console.error(err)
+        })      
+    },
+    fetchSuggestedFriends({commit}:any){
+      axios.get('/users', {
+        params : {
+          is_suggested_friend : true
+        }
+      })
+        .then(res => {
+          commit('SET_SUGGESTED_FRIENDS', res.data)
         })
         .catch(err => {
           console.error(err);
