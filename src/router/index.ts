17d3cfdb3c2cb331a85236b060f71403262983e5
christianngouw <christiangouw168@gmail.com>
2021-09-26 23:09:04 +0700

    feat: add `Profile` page (work on progress) :construction:

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,19 @@
-import Home from '../views/Home.vue'
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+
+import Home from '../views/Home.vue'
+import Profile from '../views/Profile.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'Home',
     component: Home
-  }  
+  },
+  {
+    path : '/profile',
+    name : 'Profile',
+    component : Profile
+  }
 ]
 
 const router = createRouter({
---
+++ src/views/Profile.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    Profile page
+  </div>
+</template>
+
+<script>
+import { useStore } from "vuex";
+
+import { onMounted, onUnmounted } from "vue";
+
+export default {
+  name: "Profile",
+  setup() {
+    const store = useStore();
+    store.dispatch("app-store/rightSidebarToogle", false);
+    onUnmounted(() => {
+      store.dispatch("app-store/rightSidebarToogle", true);
+    });
+  },
+};
+</script>
+
+<style></style>
