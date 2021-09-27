52931849ae172b70d908a1c7eba026a6236ee6d2
christianngouw <christiangouw168@gmail.com>
2021-09-27 23:46:58 +0700

    fix(UI): add `PremiumBanner` Component to `Profile` page and remove not used file :fire:

---
+++ src/components/PremiumBanner.vue
@@ -0,0 +1,40 @@
+<template>
+  <div
+    class="bg-indigo-500  bg-opacity-70 px-3 py-4 rounded border-l-8 border-indigo-500 flex justify-between items-center"
+  >
+    <div>
+      <div class="text-lg font-medium text-white">Become a member</div>
+      <div class="text-sm text-gray-200 mt-1">
+        Activate VIP membership to enjoy exclusive privileges.
+      </div>
+    </div>
+    <div>
+      <button
+        class="p-1 rounded-md hover:bg-white group hover:bg-opacity-30 duration-200"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-5 w-5 text-white"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M6 18L18 6M6 6l12 12"
+          />
+        </svg>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "premium-banner",
+};
+</script>
+
+<style></style>
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,16 +1,19 @@
 <template>
-  <div>
-    Profile page
+  <div class="rounded-xl py-8 px-5 bg-indigo-50 h-full">
+    <PremiumBanner />
   </div>
 </template>
 
 <script>
 import { useStore } from "vuex";
-
 import { onMounted, onUnmounted } from "vue";
+import PremiumBanner from "@/components/PremiumBanner";
 
 export default {
   name: "Profile",
+  components: {
+    PremiumBanner,
+  },
   setup() {
     const store = useStore();
     store.dispatch("app-store/rightSidebarToogle", false);
