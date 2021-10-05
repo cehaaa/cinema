4e8d5c6f701d69c335668a3795d480d5b505d344
christianngouw <christiangouw168@gmail.com>
2021-10-05 22:37:10 +0700

    fix(UI): update new component UI :art:

--- src/components/BestMovies.vue
+++ src/components/BestMovies.vue
@@ -1,144 +1,56 @@
 <template>
-  <div class="bg-white rounded-2xl p-5">
+  <div class="">
     <div class="flex items-center justify-between">
       <!-- heading -->
-      <div>
-        <div class="text-2xl font-semibold">Best Movies 🎬</div>
-        <div class="text-gray-500 mt-2">
-          100+ Movies found.
-        </div>
-      </div>
-
-      <!-- filters -->
-      <div
-        class="flex space-x-2 border border-indigo-500 rounded-lg p-2 cursor-pointer"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-6 w-6 text-indigo-500"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
+      <div class="flex items-center justify-between w-full">
+        <div class="text-2xl font-semibold">Popular Movies</div>
+        <div
+          class="text-gray-500 mt-2 cursor-pointer font-medium hover:text-pink-600 duration-200"
         >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"
-          />
-        </svg>
-        <span class=" text-indigo-500">Filters</span>
+          See All
+        </div>
       </div>
     </div>
 
     <!-- movies list -->
     <div class="mt-6">
-      <div class="divide-y divide-gray-200 divide-solid ">
+      <div class="grid grid-cols-4 gap-4">
         <div
-          class="flex justify-between items-center py-3"
           v-for="movie in movies"
-          :key="movie.id"
+          :key="movie"
+          class="overflow-hidden group cursor-pointer"
         >
-          <div class="flex space-x-5 items-center ">
-            <div class="">
-              <img
-                :src="
-                  'http://localhost:3000/storage/banner/' + movie.movie_banner
-                "
-                alt=""
-                class="object-cover object-top h-28 w-28 rounded-xl cursor-pointer"
-              />
-            </div>
-            <div>
-              <div class="font-bold text-gray-700 text-lg">
-                {{ movie.title }}
-              </div>
-              <div class="flex items-center mt-3 space-x-4">
-                <div class="flex space-x-1 items-center">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="h-5 w-5 text-gray-500"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
-                    />
-                  </svg>
-                  <span
-                    class="text-sm text-gray-500 font-medium tracking-wide"
-                    >{{ movie.duration }}</span
-                  >
-                </div>
-                <div class="flex space-x-1 items-center">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="h-5 w-5 text-gray-500"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                    />
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
-                    />
-                  </svg>
-                  <span class="text-sm text-gray-500 font-medium tracking-wide">
-                    {{ movie.total_watch }}
-                  </span>
-                </div>
+          <div class="relative overflow-hidden rounded-md">
+            <img
+              :src="
+                'http://localhost:3000/storage/banner/' + movie.movie_banner
+              "
+              class="object-cover object-center w-full rounded-md group-hover:scale-110 duration-200 transform"
+            />
+            <div class="absolute left-0 top-0 p-2">
+              <div
+                class="bg-gray-700 bg-opacity-80 rounded-md px-2 py-1 text-xs w-14 flex font-medium items-center justify-between"
+              >
+                <div>{{ movie.total_watch }}</div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-4 w-4 text-yellow-400"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+                  />
+                </svg>
               </div>
             </div>
           </div>
-          <div>
-            <button
-              class="bg-indigo-500 hover:bg-indigo-600 duration-200 text-white rounded-xl px-3 py-2 w-28 h-12 flex space-x-2 items-center justify-center"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"
-                />
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-              <span class="font-medium">Watch</span>
-            </button>
+          <div class="mt-2">
+            <div class="font-medium">{{ movie.title }}</div>
+            <div class="text-sm text-gray-500 mt-1">{{ movie.category }}</div>
           </div>
         </div>
       </div>
-    </div>
-
-    <div class="mt-5">
-      <a
-        href=""
-        class="text-indigo-500 font-medium hover:text-indigo-600 duration-200 text-base"
-        >Find more</a
-      >
     </div>
 
     <Alert
