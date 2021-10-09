f35193ca132b5166e0fff94edc09e3e2664169df
christianngouw <christiangouw168@gmail.com>
2021-10-09 22:37:14 +0700

    fix: update new theme for now showing section :hammer:

--- src/components/NowShowing.vue
+++ src/components/NowShowing.vue
@@ -1,72 +1,63 @@
 <template>
-  <div class="bg-white rounded-2xl p-5">
+  <div>
     <div class="flex items-center justify-between">
-      <!-- heading -->
-      <div>
-        <div class="text-2xl font-semibold">Now Showing</div>
-      </div>
-      <div>
-        <a
-          href=""
-          class="text-indigo-500 font-medium hover:text-indigo-600 text-base"
-          >Find more</a
-        >
+      <div class="text-2xl font-semibold">Popular Movies</div>
+      <div
+        class="text-gray-500 mt-2 cursor-pointer font-medium hover:text-pink-600 duration-200"
+      >
+        See All
       </div>
     </div>
     <div class="mt-6">
-      <div class="grid grid-cols-1 sm:grid-cols-3 gap-3">
+      <div class="grid grid-cols-1 sm:grid-cols-4 gap-4">
         <div
-          class="cursor-pointer"
+          class="overflow-hidden group cursor-pointer"
           v-for="(movie, index) in movies"
           :key="movie[index]"
         >
-          <div class="overflow-hidden rounded-lg relative">
+          <div class="relative overflow-hidden rounded-md">
             <img
               :src="
                 'http://localhost:3000/storage/banner/' + movie.movie_banner
               "
-              class="w-full object-cover object-top md:object-center h-80 lg:h-full"
+              class="object-cover object-center w-full rounded-md"
             />
-
-            <button
-              class="p-2 rounded-md bg-indigo-500 group bg-opacity-80 duration-200 absolute top-0 right-0 m-3"
-              @click="addToWatchLater(movie)"
+            <div
+              class="absolute w-full p-2 bg-gradient-to-t from-black text-base -bottom-12 group-hover:bottom-0 duration-200 flex justify-between items-center"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-5 w-5 text-white"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M12 4v16m8-8H4"
-                />
-              </svg>
-            </button>
-
-            <div class="absolute p-3 bottom-0 w-full">
-              <button
-                class="bg-indigo-500 hover:bg-indigo-600 duration-200 text-white font-medium w-full py-2 rounded"
-              >
-                Watch now
-              </button>
+              <div class="font-medium">{{ movie.title }}</div>
+              <div>
+                <button
+                  class="bg-pink-600 p-1 rounded hover:bg-pink-700"
+                  @click="addToWatchLater(movie)"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-4 w-4"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z"
+                    />
+                  </svg>
+                </button>
+              </div>
             </div>
           </div>
-          <div class="p-2">
-            <div class="flex justify-between items-center">
-              <div
-                class="font-bold text-gray-700 text-base group-hover:text-indigo-500"
-              >
-                {{ movie.title }}
-              </div>
-              <div class="flex space-x-1">
+          <div class="mt-2 flex text-xs items-center justify-between">
+            <div class="text-gray-500 tracking-wide ">
+              {{ movie.category }}
+            </div>
+            <div class="flex items-center space-x-3">
+              <div class="flex items-center space-x-1">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  class="h-5 w-5 text-yellow-400"
+                  class="h-4 w-4 text-yellow-400"
                   viewBox="0 0 20 20"
                   fill="currentColor"
                 >
@@ -74,11 +65,31 @@
                     d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
                   />
                 </svg>
-                <span class="text-sm"> {{ movie.rating }} </span>
+                <div>{{ movie.rating }}</div>
               </div>
-            </div>
-            <div class="text-gray-500 text-sm mt-1 tracking-wide">
-              {{ movie.category }}
+              <div class="flex items-center space-x-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-4 w-4"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                  />
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+                  />
+                </svg>
+                <div>{{ movie.total_watch }}</div>
+              </div>
             </div>
           </div>
         </div>
