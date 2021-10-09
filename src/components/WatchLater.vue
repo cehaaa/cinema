3c501651c0fae0c24c5f3f9348d1d4d923b22b27
christianngouw <christiangouw168@gmail.com>
2021-10-10 00:13:04 +0700

    fix: update new theme for watch later setting :hammer:

--- src/components/WatchLater.vue
+++ src/components/WatchLater.vue
@@ -2,52 +2,47 @@
   <div>
     <div class="text-2xl font-medium">Watch Later</div>
 
-    <div class="grid grid-cols-3 gap-5 mt-4" v-if="watchLater.length > 0">
-      <div v-for="movie in watchLater" :key="movie">
-        <div class="cursor-pointer relative">
+    <div
+      class="grid grid-cols-1 sm:grid-cols-3 gap-4 mt-4"
+      v-if="watchLater.length > 0"
+    >
+      <div v-for="movie in watchLater" :key="movie.id">
+        <div class="relative overflow-hidden rounded-md group">
           <img
             :src="'http://localhost:3000/storage/banner/' + movie.movie_banner"
-            class="w-full object-cover objece-center"
+            class="object-cover object-center w-full rounded-md"
           />
-          <button
-            class="p-2 rounded-md bg-indigo-500 group bg-opacity-80 duration-200 absolute top-0 right-0 m-3"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-5 w-5 text-white"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-              />
-            </svg>
-          </button>
           <div
-            class="absolute bottom-0 p-3 w-full text-white bg-gradient-to-t from-gray-800 flex justify-between items-center"
+            class="absolute top-0 group-hover:right-0 -right-12 m-3 duration-200 opacity-0 group-hover:opacity-100"
           >
-            <div class="font-bold text-lg">{{ movie.title }}</div>
-            <div class="flex space-x-1 items-center">
+            <button
+              class="bg-gray-700 hover:bg-gray-600 hover:bg-opacity-80 duration-200 rounded-full p-1 bg-opacity-80"
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                class="h-5 w-5 text-yellow-500"
-                viewBox="0 0 20 20"
-                fill="currentColor"
+                class="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
               >
                 <path
-                  d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M6 18L18 6M6 6l12 12"
                 />
               </svg>
-              <span class="font-medium"> {{ movie.rating }} </span>
-            </div>
+            </button>
+          </div>
+          <div
+            class="absolute p-3 font-bold bg-gradient-to-t from-black w-full bottom-0 text-lg"
+          >
+            {{ movie.title }}
           </div>
         </div>
       </div>
     </div>
+
     <div v-else class="flex mt-4 justify-center items-center">
       <div class="text-center flex items flex-col items-center space-y-2">
         <img src="@/assets/img/empty_history.png" alt="" />
@@ -58,7 +53,7 @@
           </div>
           <router-link
             to="/"
-            class="p-3 inline-block mt-9 text-white font-medium rounded-lg bg-indigo-500 hover:bg-indigo-600 duration-200"
+            class="p-3 inline-block mt-9 text-white font-medium rounded-lg bg-pink-600 hover:bg-pink-500 duration-200"
           >
             Watch Movies Now!
           </router-link>
